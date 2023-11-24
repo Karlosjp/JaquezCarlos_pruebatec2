95807d71b415a7a4233a0212a9c07e9123331b75
Karlosjp <carlosjpinfor@gmail.com>
2023-11-24 01:55:04 +0100

    Inicio
    Conexion a bbdd "turnero"

---
+++ gestionturnos/src/main/java/ar/pruebatec2/gestionturnos/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package ar.pruebatec2.gestionturnos;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ gestionturnos/src/main/java/ar/pruebatec2/gestionturnos/extras/Estados.java
@@ -0,0 +1,14 @@
+package ar.pruebatec2.gestionturnos.extras;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public enum Estados {
+    TERMINADO,
+    ACTIVO,
+    CANCELADO
+
+}
